793fa37a98848044df4a66077ebf1e088b496a1f
sian2002 <arthur.tangi02@gmail.com>
2026-05-09 21:47:28 +1000

    Exercise4 W01

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number and enter 0 to quit. ");
+
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        int total = 0;
+        foreach (int number in numbers)
+        {
+            total = total + number;
+        }
+
+        Console.WriteLine($"The sum is {total}");
+
+        float average = ((float)total) / numbers.Count;
+        Console.WriteLine($"The average is {average}");
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is {max}");
     }
 }
