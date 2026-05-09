eebccb7127f311c0d01ac28584c7513948a81e6e
sian2002 <arthur.tangi02@gmail.com>
2026-05-09 19:52:07 +1000

    Exercise2 W02

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int grade = int.Parse(answer);
+
+        string letter = "";
+        
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (grade < 70)
+        {
+            Console.WriteLine("You failed. Please try again. ");
+        }
+
+        else
+        {
+            Console.WriteLine("Congrats, you passed. ");
+        }
     }
 }
