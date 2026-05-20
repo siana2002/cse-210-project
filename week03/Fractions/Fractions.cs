536258e9a7add67805b3bde6cf6fa769891f8585
sian2002 <arthur.tangi02@gmail.com>
2026-05-20 17:04:42 +1000

    W03 Encapsulation Activity

---
+++ week03/Fractions/Fractions.cs
@@ -0,0 +1,37 @@
+using System;
+
+public class Fractions
+{
+    private int _top;
+    private int _bottom;
+
+    public Fractions()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fractions(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fractions(int top , int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string text = $"{_top}, {_bottom}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        double result = (double)_top / (double)_bottom;
+        return result;
+    }
+}
